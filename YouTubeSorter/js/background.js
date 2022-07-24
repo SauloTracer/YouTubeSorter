9baa27e97e145992121312e4bd9aa2f02cad92d8
Saulo Machado <saulo.tracer@gmail.com>
2022-07-24 14:04:02 -0300

    Add files via upload

--- YouTubeSorter/js/background.js
+++ YouTubeSorter/js/background.js
@@ -8,28 +8,42 @@ function totalDurationInSeconds(duration) {
 }
 
 async function sort() {
-    const queryOptions = {
-        url: [
-            "http://www.youtube.com/watch?*",
-            "https://www.youtube.com/watch?*"
-        ],
-        currentWindow: true
-    };
-    const tabs = await chrome.tabs.query(queryOptions);
+    try{
+        const queryOptions = {
+            url: [
+                "http://www.youtube.com/watch?*",
+                "https://www.youtube.com/watch?*",
+                "http://www.youtube.com/shorts*",
+                "https://www.youtube.com/shorts*"            
+            ],
+            currentWindow: true
+        };
+        const tabs = await chrome.tabs.query(queryOptions);
 
-    const data = await Promise.all(
-        tabs.map(async x => {
-            const response = await chrome.tabs.sendMessage(x.id, { action: "sort", txt: "Hello from bg" }); 
-            if (!response) return
-            x.durationInSeconds = totalDurationInSeconds(response.duration);
-            return x
-        })
-    )
+        let data = await Promise.allSettled(
+            tabs.map(async x => {
+                const response = await chrome.tabs.sendMessage(x.id, { action: "sort" }); 
+                if (!response) return
+                x.durationInSeconds = totalDurationInSeconds(response.duration);
+                return x
+            })
+        )
 
-    data.sort((a, b) => a.durationInSeconds - b.durationInSeconds)
+        data = data.filter(response => response.status === 'fulfilled')
+        data = data.map(response => response.value)
+            .sort((a, b) => a.durationInSeconds - b.durationInSeconds)
 
-    for (let index = 0; index < data.length; index++) {
-        await chrome.tabs.move(data[index].id, { index });
+        for (let index = 0; index < data.length; index++) {
+            try {
+                await chrome.tabs.move(data[index].id, { index });
+            }
+            catch(e) {
+                console.log(e)
+                continue
+            }
+        }
+    } catch(e) {
+        console.log(e)
     }
 }
 
--- YouTubeSorter/js/content.js
+++ YouTubeSorter/js/content.js
@@ -1,9 +1,8 @@
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    console.log("Message received: ", request.txt);
     if (request.action == "sort") {
         console.log("Sorting...");
-        const title = document.querySelector("#container > h1 > yt-formatted-string").textContent
-        const duration = document.querySelector(".ytp-time-duration").textContent
-        sendResponse({ title, duration });
+        const videoType = window.location.href.includes("/shorts/") ? "shorts" : "watch";
+        const duration = videoType == "watch" ? document.querySelector(".ytp-time-duration").textContent : document.querySelector(".html5-main-video").duration.toString()
+        sendResponse({ duration });
     }
 });
